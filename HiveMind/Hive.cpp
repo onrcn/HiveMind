f57e73f1fc4ce977f7a55f7a28769d34d92b1dd5
Onurcan Erenel <onrcnernl@gmail.com>
2021-12-26 21:48:34 +0300

    Addition to the Bee and Hives

--- HiveMind/Hive.cpp
+++ HiveMind/Hive.cpp
@@ -1,1 +1,17 @@
 #include "Hive.h"
+
+Hive::Hive()
+{
+}
+
+Hive::Hive(int size, int price)
+{
+}
+
+Hive::Hive(int size, std::vector<HoneyBee> honeybees, std::vector<DroneBee> malebees)
+{
+}
+
+Hive::~Hive()
+{
+}
--- HiveMind/Hive.h
+++ HiveMind/Hive.h
@@ -11,5 +11,10 @@ private:
 	Bee queen;
 	int size;
 	int price;
+public:
+	Hive();
+	Hive(int size, int price);
+	Hive(int size, std::vector<HoneyBee> honeybees, std::vector<DroneBee> malebees);
+	virtual ~Hive();
 };
 
